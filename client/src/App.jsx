78903551ddade9130c901b875597b273bd6fc60a
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-18 11:31:45 +1300

    header ui of chatbot doneee

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import tinaLogo from "./assets/tina.svg";
+import refreshLogo from "./assets/refresh.svg";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        {/* Header */}
+        <div className="header">
+          <img src={tinaLogo} alt="Tina Logo" className="logo" />
+
+          <div className="header-text">
+            <div className="title">
+              Tina - Your AI Insurance Policy Assistant
+            </div>
+            <div className="status">
+              <span className="dot"></span> Online Now
+            </div>
+          </div>
+          <img src={refreshLogo} alt="Refresh" className="refresh-logo" />
+        </div>
+
+        <div>
+          {/* this div is for the chats, will be scrollable, auto scrolls to latest message */}
+        </div>
+
+        <div>
+          {/* user input div, where user user can input message and hit sent / enter */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

b0b3f77076b2e8193e6b95fbb8f259db1a7aaa36
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-18 18:45:17 +1300

    initial backend set up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello Nerds!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
